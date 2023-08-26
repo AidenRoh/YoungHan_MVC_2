ad58ba493b45ab181e3b2ccb0067376c3a81c569
AidenRoh <colohrby@gmail.com>
2023-08-26 17:00:50 +0900

    message

--- message/src/main/java/hello/itemservice/web/message/MessageItemController.java
+++ message/src/main/java/hello/itemservice/web/message/MessageItemController.java
@@ -1,100 +1,61 @@
-package hello.itemservice.web.form;
+package hello.itemservice.web.message;
 
-import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
-import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
-@Slf4j
 @Controller
-@RequestMapping("/form/items")
+@RequestMapping("/message/items")
 @RequiredArgsConstructor
-public class FormItemController {
+public class MessageItemController {
 
     private final ItemRepository itemRepository;
 
-    @ModelAttribute("regions")
-    public Map<String, String> regions() {
-        Map<String, String> regions = new LinkedHashMap<>();
-        regions.put("SEOUL", "서울");
-        regions.put("BUSAN", "부산");
-        regions.put("JEJU", "제주");
-        return regions;
-    }
-
-    @ModelAttribute("itemTypes")
-    public ItemType[] itemTypes() {
-        return ItemType.values();
-    }
-
-    @ModelAttribute("deliveryCodes")
-    public List<DeliveryCode> deliveryCodes() {
-        List<DeliveryCode> deliveryCodes = new ArrayList<>();
-        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
-        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
-        deliveryCodes.add(new DeliveryCode("SLOQ", "느린 배송"));
-        return deliveryCodes;
-    }
-
-
     @GetMapping
-
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
-        return "form/items";
+        return "message/items";
     }
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "form/item";
+        return "message/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
-        return "form/addForm";
+        return "message/addForm";
     }
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
-
-        log.info("item.open={}", item.getOpen());
-        log.info("item.regions={}", item.getRegions());
-        log.info("item.itemType={}", item.getItemType());
-
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
-        return "redirect:/form/items/{itemId}";
+        return "redirect:/message/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "form/editForm";
+        return "message/editForm";
     }
 
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
-        return "redirect:/form/items/{itemId}";
+        return "redirect:/message/items/{itemId}";
     }
 
 }
